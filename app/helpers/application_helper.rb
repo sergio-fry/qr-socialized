17abb64913d6b2e6d3690d9d9668b8aacc5427af
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-04-06 13:15:27 +0300

    markdown, qr

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def qr_code_for(data, options={})
+    options[:size] ||= 150
+
+    image_tag "//chart.googleapis.com/chart?cht=qr&chs=#{options[:size]}x#{options[:size]}&chl=#{data}"
+  end
 end
--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,7 @@
 module PagesHelper
+  def page_body_format(text)
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new)
+
+    raw markdown.render(text)
+  end
 end
