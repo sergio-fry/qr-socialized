2d2e6eccca70124218c30bbf4bcb81c9bab27799
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-04-07 10:44:06 +0300

    qr на главной

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
   def qr_code_for(data, options={})
-    options[:size] ||= 150
+    width = height = (options[:size] || 150)
 
-    image_tag "//chart.googleapis.com/chart?cht=qr&chs=#{options[:size]}x#{options[:size]}&chl=#{data}"
+    image_tag "//chart.googleapis.com/chart?cht=qr&chs=#{width}x#{height}&chl=#{URI::encode(data)}"
   end
 end
